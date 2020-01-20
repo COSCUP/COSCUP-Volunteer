216c1b65776434431c9a33bd40ec9d172c40f484
Toomore Chiang <toomore0929@gmail.com>
2020-01-20 15:09:03 +0000

    Add setting, user page, but not completed

---
+++ module/project_working.py
@@ -0,0 +1,11 @@
+from models.teamdb import TeamDB
+from module.project import Project
+from module.team import Team
+
+
+if __name__ == '__main__':
+    #Project.create(pid='2020', name='COSCUP 2020', owners=['6c74e623', ], action_date='2020-08-01')
+    #Team.create(pid='2020', tid='secretary', name=u'行政組', owners=['6c74e623', ])
+    #TeamDB(None, None).index()
+    #Team.update_chiefs(pid='2020', tid='secretary', add_uids=['6c74e623', ])
+    pass
