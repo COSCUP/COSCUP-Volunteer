e74d781fb75d681c66361ce93b0e200930bbb199
Toomore Chiang <toomore0929@gmail.com>
2020-01-30 13:01:56 +0000

    Complete appreciation back-end

--- models/index.py
+++ models/index.py
@@ -1,9 +1,11 @@
+from models.formdb import FormDB
 from models.projectdb import ProjectDB
 from models.teamdb import TeamDB
 from models.waitlistdb import WaitListDB
 
 
 if __name__ == '__main__':
+    FormDB().index()
     ProjectDB(pid=None).index()
     TeamDB(pid=None, tid=None).index()
     WaitListDB().index()
