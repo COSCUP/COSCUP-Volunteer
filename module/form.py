efd207378dd99dbc05d18e08c247e9136c3461f4
Toomore Chiang <toomore0929@gmail.com>
2020-01-31 13:45:15 +0000

    Complete traffic fee back-end

--- module/form.py
+++ module/form.py
@@ -3,6 +3,25 @@ from models.formdb import FormDB
 
 class Form(object):
     ''' Form Object '''
+    TRAFFIC_FEE_LOCATIONS = (
+        ('基隆', 114),
+        ('大台北地區', 110),
+        ('宜蘭', 380),
+        ('桃園', 162),
+        ('新竹', 320),
+        ('苗栗', 420),
+        ('臺中', 640),
+        ('彰化', 690),
+        ('南投', 790),
+        ('雲林', 850),
+        ('嘉義', 870),
+        ('臺南', 930),
+        ('高雄', 1120),
+        ('屏東', 1200),
+        ('花蓮', 880),
+        ('臺東', 1566),
+        ('離島', 1200),
+    )
 
     @staticmethod
     def update_appreciation(pid, uid, data):
@@ -55,3 +74,31 @@ class Form(object):
 
         '''
         return FormDB().find_one({'case': 'volunteer_certificate', 'pid': pid, 'uid': uid})
+
+    @staticmethod
+    def update_traffic_fee(pid, uid, data):
+        ''' Update traffic fee
+
+        :param str pid: project id
+        :param str uid: user id
+        :param dict data: form data
+
+        .. note::
+            - data:
+                - howto: str
+                - apply: bool
+                - fromwhere: str
+                - fee: int
+
+        '''
+        return FormDB().add_by_case(case='traffic_fee', pid=pid, uid=uid, data=data)
+
+    @staticmethod
+    def get_traffic_fee(pid, uid):
+        ''' Get traffic fee
+
+        :param str pid: project id
+        :param str uid: user id
+
+        '''
+        return FormDB().find_one({'case': 'traffic_fee', 'pid': pid, 'uid': uid})
