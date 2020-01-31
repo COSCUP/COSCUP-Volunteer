23c3e0e242a4a685f129fc5e81421b55c2b30af3
Toomore Chiang <toomore0929@gmail.com>
2020-01-31 12:21:17 +0000

    Complete volunteer_certificate back-end

--- module/form.py
+++ module/form.py
@@ -30,3 +30,28 @@ class Form(object):
 
         '''
         return FormDB().find_one({'case': 'appreciation', 'pid': pid, 'uid': uid})
+
+    @staticmethod
+    def update_volunteer_certificate(pid, uid, data):
+        ''' Update volunteer certificate
+
+        :param str pid: project id
+        :param str uid: user id
+        :param dict data: form data
+
+        .. note::
+            - data:
+                - value: bool
+
+        '''
+        return FormDB().add_by_case(case='volunteer_certificate', pid=pid, uid=uid, data=data)
+
+    @staticmethod
+    def get_volunteer_certificate(pid, uid):
+        ''' Get volunteer certificate
+
+        :param str pid: project id
+        :param str uid: user id
+
+        '''
+        return FormDB().find_one({'case': 'volunteer_certificate', 'pid': pid, 'uid': uid})
