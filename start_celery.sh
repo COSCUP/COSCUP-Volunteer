17ad450c4f23e4fa8ac0b3662742fe2b7f4f9f19
Toomore Chiang <toomore0929@gmail.com>
2020-02-12 03:04:11 +0000

    Update rabbitmq to 3.8.2

--- start_celery.sh
+++ start_celery.sh
@@ -1,8 +1,8 @@
 docker run -d --restart='always' \
            --name secretary_celery \
            --link secretary_mongo:mongo \
-           --link queue_sender:celery \
+           --link queue_sender:rabbitmq \
            --log-opt max-size=64m \
            --log-opt max-file=1 \
            -v $(pwd)/log_docker_celery:/app/log_docker_celery \
-           secretary-app:prod env C_FORCE_ROOT=true celery -A celery_task worker -B -l info -O fair -c 4 --logfile ./log_docker_celery/log.log
+           secretary-app:prod env C_FORCE_ROOT=true celery -A celery_task worker -B -l info -O fair -c 2 --logfile ./log_docker_celery/log.log
