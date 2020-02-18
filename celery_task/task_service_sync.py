f12ff9e1c0260a01f18f439d182c009563e26398
Toomore Chiang <toomore0929@gmail.com>
2020-02-18 18:37:29 +0000

    Asign in real need

--- celery_task/task_service_sync.py
+++ celery_task/task_service_sync.py
@@ -19,7 +19,7 @@ logger = get_task_logger(__name__)
     routing_key='cs.servicesync.gsuite.memberchange', exchange='COSCUP-SECRETARY')
 def service_sync_gsuite_memberchange(sender, **kwargs):
     team_member_change_db = TeamMemberChangedDB()
-    sync_gsuite = SyncGSuite(credentialfile=setting.GSUITE_JSON, with_subject=setting.GSUITE_ADMIN)
+    sync_gsuite = None
     for raw in team_member_change_db.find(
         {'done.gsuite_team': {'$exists': False}},
         sort=(('create_at', 1), )):
@@ -28,6 +28,9 @@ def service_sync_gsuite_memberchange(sender, **kwargs):
         if 'mailling' not in team or not team['mailling']:
             team_member_change_db.find_one_and_update({'_id': raw['_id']}, {'$set': {'done.gsuite_team': True}})
             continue
+
+        if sync_gsuite is None:
+            sync_gsuite = SyncGSuite(credentialfile=setting.GSUITE_JSON, with_subject=setting.GSUITE_ADMIN)
 
         user = User(uid=raw['uid']).get()
         if raw['case'] == 'add':
@@ -46,6 +49,9 @@ def service_sync_gsuite_memberchange(sender, **kwargs):
         if 'mailling_staff' not in project or not project['mailling_staff']:
             team_member_change_db.find_one_and_update({'_id': raw['_id']}, {'$set': {'done.gsuite_staff': True}})
             continue
+
+        if sync_gsuite is None:
+            sync_gsuite = SyncGSuite(credentialfile=setting.GSUITE_JSON, with_subject=setting.GSUITE_ADMIN)
 
         user = User(uid=raw['uid']).get()
         if raw['case'] == 'add':
