7207da42249c918b506094a590aef2b7d4189e22
Toomore Chiang <toomore0929@gmail.com>
2020-02-18 10:04:58 +0000

    Fixed rabbitmq assign fail in docker

--- start_worker.sh
+++ start_worker.sh
@@ -2,7 +2,7 @@ docker run \
            -it --rm \
            --name volunteer-worker \
            --link secretary_mongo:mongo \
-           --link queue_sender:celery \
+           --link queue_sender:rabbitmq \
            --log-opt max-size=64m \
            --log-opt max-file=1 \
            -v $(pwd)/scripts:/app/scripts \
