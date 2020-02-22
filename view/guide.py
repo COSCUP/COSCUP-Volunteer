b027c938a63bd89e2140c981471ddd6db2e0031e
Toomore Chiang <toomore0929@gmail.com>
2020-02-22 06:44:41 +0000

    Remove unused code

--- view/guide.py
+++ view/guide.py
@@ -1,11 +1,8 @@
 from flask import Blueprint
-from flask import g
-from flask import redirect
 from flask import render_template
-from flask import request
-from flask import url_for
 
 VIEW_GUIDE = Blueprint('guide', __name__, url_prefix='/guide')
+
 
 @VIEW_GUIDE.route('/')
 def index():
