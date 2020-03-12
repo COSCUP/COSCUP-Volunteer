057af3c6c987d6a83ab0288daefa6ef96bad81eb
Toomore Chiang <toomore0929@gmail.com>
2020-03-12 08:39:28 +0000

    Add sender content edit page

--- models/senderdb.py
+++ models/senderdb.py
@@ -48,6 +48,7 @@ class SenderCampaignDB(DBBase):
             'mail': {
                 'subject': '',
                 'content': '',
+                'preheader': '',
             },
         }
 
--- module/sender.py
+++ module/sender.py
@@ -1,4 +1,7 @@
+from pymongo.collection import ReturnDocument
+
 from models.senderdb import SenderCampaignDB
+
 
 class SenderCampaign(object):
     ''' SenderCampaign class '''
@@ -16,3 +19,37 @@ class SenderCampaign(object):
 
         data = SenderCampaignDB.new(name=name.strip(), pid=pid, tid=tid, uid=uid)
         return SenderCampaignDB().save(data)
+
+    @staticmethod
+    def get(cid, pid=None, tid=None):
+        ''' Get campaign
+
+        :param str cid: cid
+        :param str pid: pid
+        :param str tid: tid
+
+        '''
+        query = {'_id': cid}
+
+        if pid is not None:
+            query['created.pid'] = pid
+
+        if tid is not None:
+            query['created.tid'] = tid
+
+        return SenderCampaignDB().find_one(query)
+
+    def save_mail(cid, subject, content, preheader):
+        ''' Save mail data
+
+        :param str cid: cid
+        :param str subject: subject
+        :param str content: content
+        :param str preheader: preheader
+
+        '''
+        return SenderCampaignDB().find_one_and_update(
+            {'_id': cid},
+            {'$set': {'mail.subject': subject, 'mail.content': content, 'mail.preheader': preheader}},
+            return_document=ReturnDocument.AFTER,
+        )
