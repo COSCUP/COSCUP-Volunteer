1635771709fc0fae02b16c8fb324e172fe2a4de9
Toomore Chiang <toomore0929@gmail.com>
2020-06-16 07:42:16 +0000

    memcached to 1.6.6

--- start_memcached.sh
+++ start_memcached.sh
@@ -1,3 +1,3 @@
 docker run -d --restart=always --log-opt max-size=12m --log-opt max-file=1 \
            -p 127.0.0.1:11212:11211 \
-           --name memcached-prod memcached:1.6.1-alpine
+           --name memcached-prod memcached:1.6.6-alpine
