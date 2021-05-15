da362410b332e02ffe898643e696d73550ed7a34
Toomore Chiang <toomore0929@gmail.com>
2021-05-15 09:57:32 +0000

    Add expense save

---
+++ module/expense.py
@@ -0,0 +1,34 @@
+from models.expensedb import ExpenseDB
+
+
+class Expense(object):
+    ''' Expense class '''
+    @staticmethod
+    def proess_and_add(pid, tid, uid, data):
+        ''' Process data from web '''
+        save = ExpenseDB.new(pid=pid, tid=tid, uid=uid)
+
+        save['request'] = {
+                'buid': data['expense_request']['buid'],
+                'desc': data['expense_request']['desc'],
+                'paydate': data['expense_request']['paydate'],
+            }
+
+        save['bank'] = {
+                'branch': data['bank']['branch'],
+                'code': data['bank']['code'],
+                'name': data['bank']['name'],
+                'no': data['bank']['no'],
+            }
+
+        for invoice in data['invoices']:
+            save['invoices'].append(
+                    {
+                        'currency': invoice['currency'],
+                        'name': invoice['name'],
+                        'status': invoice['status'],
+                        'total': invoice['total'],
+                    })
+
+        return ExpenseDB().add(data=save)
+
