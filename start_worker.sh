2665368e0eca0315214ef37e81f4e0410bfc31b7
pan93412 <pan93412@gmail.com>
2022-03-13 15:41:23 +0800

    fix: some issues indicated by pylance

--- start_worker.sh
+++ start_worker.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
 docker run \
            -it --rm \
            --name volunteer-worker \
@@ -7,7 +10,7 @@ docker run \
            --log-opt max-size=64m \
            --log-opt max-file=1 \
            -p 6699:6699 \
-           -v $(pwd)/scripts:/app/scripts \
+           -v "$(pwd)"/scripts:/app/scripts \
            -e LD_PRELOAD=/usr/local/lib/libjemalloc.so \
            -e PYTHONPATH=/app \
            volunteer-app:prod sh
