25865def9c5af9ae8126535fb5192d16d10b0f4f
pan93412 <pan93412@gmail.com>
2022-03-13 11:45:28 +0800

    fix: use docker volume

--- start_celery.sh
+++ start_celery.sh
@@ -4,6 +4,6 @@ docker run -d --restart='always' \
            --link queue_sender:rabbitmq \
            --log-opt max-size=64m \
            --log-opt max-file=1 \
-           -v $(pwd)/log_docker_celery:/app/log_docker_celery \
+           -v log_docker_celery:/app/log_docker_celery \
            -e LD_PRELOAD=/usr/local/lib/libjemalloc.so \
            volunteer-app:prod env C_FORCE_ROOT=true celery -A celery_task.celery_main worker -B -l info -O fair -c 4 --logfile ./log_docker_celery/log.log
