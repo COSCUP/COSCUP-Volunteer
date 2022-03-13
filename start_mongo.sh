208a137ce36772c366dab7b1aee6e3cd7b2eae1b
pan93412 <pan93412@gmail.com>
2022-03-13 11:45:28 +0800

    fix: use docker volume

--- start_mongo.sh
+++ start_mongo.sh
@@ -1,4 +1,4 @@
 docker run -d --restart=always --log-opt max-size=12m --log-opt max-file=1 \
            -p 127.0.0.1:27018:27017 \
-           -v /srv/mongodb_data_secretary:/data/db \
+           -v mongodb_data_secretary:/data/db \
            --name secretary_mongo mongo:4
