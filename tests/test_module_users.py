fa013ae00417c32f931fcafbe2dc1bea9ddebc66
Toomore Chiang <toomore0929@gmail.com>
2022-03-29 16:54:42 +0800

    Add first testing from user

---
+++ tests/test_module_users.py
@@ -0,0 +1,61 @@
+''' test module/user '''
+import pytest
+
+from models.oauth_db import OAuthDB
+from module.users import User
+
+
+@pytest.fixture(scope='module', params=[None, 'coscup'])
+def uid(request):
+    ''' uid '''
+    return request.param
+
+
+@pytest.fixture(scope='module', params=[None, 'coscup@coscup.org'])
+def mail(request):
+    ''' mail '''
+    return request.param
+
+
+class TestUser:  # pylint: disable=too-few-public-methods
+    ''' Test User '''
+
+    @staticmethod
+    def test_init(uid, mail):  # pylint: disable=redefined-outer-name
+        ''' test init '''
+        user = User(uid=uid, mail=mail)
+        assert user.uid == uid
+        assert user.mail == mail
+
+    @staticmethod
+    def test_get():
+        ''' Test get '''
+        user = User(uid='coscup')
+        user.get()
+
+    @staticmethod
+    def test_create_and_oauth():
+        ''' test create user '''
+        _mail = 'coscup@coscup.org'
+        with pytest.raises(Exception) as error:
+            User.create(mail=_mail)
+
+        assert str(error.value) == f'mail: `{_mail}` not in the oauth dbs'
+
+        OAuthDB().add_data(mail=_mail, data={})
+
+        OAuthDB().find_one_and_update(
+            {'_id': _mail}, {'$set': {'owner': '00000000'}})
+
+        with pytest.raises(Exception) as error:
+            User.create(mail=_mail)
+
+        assert str(error.value) == f'mail:`{_mail}` already bind'
+
+    @staticmethod
+    def test_create_success():
+        ''' Test create user success '''
+        _mail = 'coscup+success@coscup.org'
+        OAuthDB().add_data(mail=_mail, data={})
+        created_user = User.create(mail=_mail)
+        assert created_user['mail'] == _mail
