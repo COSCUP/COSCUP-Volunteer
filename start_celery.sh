a273cafb7e6e803f70594b733da610597550a40b
Toomore Chiang <toomore0929@gmail.com>
2022-05-03 23:47:46 +0800

    Update dependencies and add for dev
    - Adject for poetry in production
    - Using snok/install-poetry for actions
    - Python version down to 3.8
    - Fixed lost using poetry run

--- start_celery.sh
+++ start_celery.sh
@@ -6,4 +6,4 @@ docker run -d --restart='always' \
            --log-opt max-file=1 \
            -v $(pwd)/log_docker_celery:/app/log_docker_celery \
            -e LD_PRELOAD=/usr/local/lib/libjemalloc.so \
-           volunteer-app:prod env C_FORCE_ROOT=true celery -A celery_task.celery_main worker -B -l info -O fair -c 4 --logfile ./log_docker_celery/log.log
+           volunteer-app:prod env C_FORCE_ROOT=true poetry run celery -A celery_task.celery_main worker -B -l info -O fair -c 4 --logfile ./log_docker_celery/log.log
