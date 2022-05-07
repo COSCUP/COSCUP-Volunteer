3e694a62b44fc50d7b2e981db0c5aa7c73666276
Toomore Chiang <toomore0929@gmail.com>
2022-05-07 11:34:58 +0000

    Upgrade memcached, mongodb, rabbitmq

--- start_rabbitmq.sh
+++ start_rabbitmq.sh
@@ -2,4 +2,4 @@ docker run -d --restart=always --log-opt max-size=12m --log-opt max-file=1 \
            -p 127.0.0.1:5673:5672 \
            -p 127.0.0.1:15673:15672 \
            -v /srv/queue_sender:/var/lib/rabbitmq \
-           --name queue_sender rabbitmq:3.8.5-management-alpine
+           --name queue_sender rabbitmq:3.8.30-management-alpine
