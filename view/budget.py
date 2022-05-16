7727ff5e1447822af661817ee607483123f1a4f1
Toomore Chiang <toomore0929@gmail.com>
2022-05-16 06:54:59 +0000

    Add batch upload budget
    issue #63

--- view/budget.py
+++ view/budget.py
@@ -1,4 +1,7 @@
 ''' Budget '''
+import csv
+import io
+
 from flask import Blueprint, g, jsonify, redirect, render_template, request
 
 from module.budget import Budget
@@ -6,6 +9,72 @@ from module.project import Project
 from module.team import Team
 
 VIEW_BUDGET = Blueprint('budget', __name__, url_prefix='/budget')
+
+
+@VIEW_BUDGET.route('/batch/<pid>', methods=('GET', 'POST'))
+def batch(pid):
+    ''' batch upload '''
+    project = Project.get(pid)
+
+    if not project:
+        return redirect('/')
+
+    is_admin = Budget.is_admin(pid=pid, uid=g.user['account']['_id'])
+    if not is_admin:
+        return redirect('/')
+
+    if request.method == 'GET':
+        return render_template('./budget_batch.html', project=project, is_admin=is_admin)
+
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+
+            if data and 'casename' in data and data['casename'] == 'get':
+                teams = []
+                for team in Team.list_by_pid(pid=project['_id']):
+                    teams.append({'name': team['name'], 'tid': team['tid']})
+
+                return jsonify({'teams': teams})
+
+        if request.files and 'file' in request.files:
+            csv_file = list(csv.DictReader(io.StringIO('\n'.join(
+                request.files['file'].read().decode('utf8').split('\n')[1:]))))
+
+            result, error_result = Budget.verify_batch_items(items=csv_file)
+
+            dedup_result = []
+            dup_bids = []
+            for item in result:
+                if item['action'] == 'add' and Budget.get_by_bid(pid=pid, bid=item['bid']):
+                    dup_bids.append(item['bid'])
+                    continue
+
+                if item['action'] == 'update' and not Budget.get_by_bid(pid=pid, bid=item['bid']):
+                    dup_bids.append(item['bid'])
+                    continue
+
+                dedup_result.append(item)
+
+            if request.form['casename'] == 'verify':
+                return jsonify({
+                    'file': csv_file,
+                    'confirmed': dedup_result,
+                    'error_items': error_result,
+                    'dup_bids': dup_bids,
+                })
+
+            if request.form['casename'] == 'save':
+                for item in dedup_result:
+                    if item['action'] == 'add':
+                        Budget.add(pid=pid, tid=item['tid'], data=item)
+                    elif item['action'] == 'update':
+                        budget_id = Budget.get_by_bid(pid=pid, bid=item['bid'])
+                        if budget_id:
+                            item['_id'] = budget_id
+                            Budget.edit(pid=pid, data=item)
+
+    return jsonify({})
 
 
 @VIEW_BUDGET.route('/<pid>', methods=('GET', 'POST'))
