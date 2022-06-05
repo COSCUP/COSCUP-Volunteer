1aaba087989ee69176bcc75338ab16f3a4d45eda
Toomore Chiang <toomore0929@gmail.com>
2022-06-05 13:10:29 +0000

    Fixed the return data and add more tips

--- view/team.py
+++ view/team.py
@@ -753,12 +753,13 @@ def team_form_parking_card(pid, tid):
         if post_data['casename'] == 'get':
             data = Form.get_parking_card(
                 pid=team['pid'], uid=g.user['account']['_id'])
-            if not data:
-                return jsonify({'data': {'carno': '', 'dates': []}})
 
             parking_card_options = []
             if 'parking_card' in project:
                 parking_card_options = project['parking_card']
+
+            if not data:
+                return jsonify({'data': {'carno': '', 'dates': []}, 'parking_card_options': parking_card_options})
 
             return jsonify({'data': data['data'], 'parking_card_options': parking_card_options})
 
