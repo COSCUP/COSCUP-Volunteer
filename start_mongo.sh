7d1f11b7869892164ead2ca8b24575b72c48c035
Toomore Chiang <toomore0929@gmail.com>
2023-04-09 01:19:10 +0800

    Fixed mongodb version
    Signed-off-by: Toomore Chiang <toomore0929@gmail.com>

--- start_mongo.sh
+++ start_mongo.sh
@@ -1,4 +1,4 @@
 docker run -d --restart=always --log-opt max-size=12m --log-opt max-file=1 \
            -p 127.0.0.1:27018:27017 \
            -v /srv/mongodb_data_secretary:/data/db \
-           --name secretary_mongo mongo:6.0.5-focal
+           --name secretary_mongo mongo:6.0.5-jammy
